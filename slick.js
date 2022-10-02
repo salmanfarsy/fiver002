9798b7181fff135e35d8312a8e6e9c3672e5ba64
salmanfarsy <blieveburn007@gmail.com>
2022-10-02 19:12:34 +0600

    done with reponsive

--- slick.js
+++ slick.js
@@ -4,34 +4,34 @@ $('.items').slick({
             dots: false,
             fade: true,
             arrows: true,
-            prevArrow: '<span class="prev"><i class="flaticon-arrow"></i></span"></i> prev </span>',
-            nextArrow: '<span class="next"><i class="flaticon-right-arrow"></i>Next</span>',
+            prevArrow: '<span class="prev"> > </span>',
+            nextArrow: '<span class="next"> < </span>',
             responsive: [
                 {
                     breakpoint: 768,
                     settings: {
-                        arrows: false
+                        arrows: true
                     }
                 }
             ]
         });
 
 $('.items2').slick({
-            autoplay: true,
-            autoplaySpeed: 5000,
-            dots: false,
-            fade: true,
-            arrows: true,
-            prevArrow: '<span class="prev"><i class="flaticon-arrow"></i></span"></i> prev </span>',
-            nextArrow: '<span class="next"><i class="flaticon-right-arrow"></i>Next</span>',
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        arrows: false
-                    }
-                }
-            ]
-        });
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
+    fade: true,
+    arrows: true,
+    prevArrow: '<span class="prev"> > </span>',
+    nextArrow: '<span class="next"> < </span>',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
+});
     
 	
