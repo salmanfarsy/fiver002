03e7e00ed7877017cdbffd688e28e7ddb8ee6a6e
salmanfarsy <blieveburn007@gmail.com>
2022-10-14 19:31:53 +0600

    gonna add a footer

--- slick.js
+++ slick.js
@@ -17,23 +17,24 @@ $('.items').slick({
                 }
             ]
         });
-
-$('.items2').slick({
-    autoplay: true,
-    autoplaySpeed: 5000,
-    dots: false,
-    fade: true,
-    arrows: true,
-    prevArrow: '<span class="prev"> > </span>',
-    nextArrow: '<span class="next"> < </span>',
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false
-            }
-        }
-    ]
-});
+        $('.items2').slick({
+            slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: true,
+                    autoplay: true,
+                    autoplaySpeed: 5000,
+                    dots: false,
+                    arrows: true,
+                    prevArrow: '<span class="prev"> < </span>',
+                    nextArrow: '<span class="next"> > </span>',
+                    responsive: [
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                arrows: true
+                            }
+                        }
+                    ]
+                });
     
 	
